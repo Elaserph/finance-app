9ff0df310f601f6693d84a4fa529ab13e458fc0b
shreepal shekhawat <shreepal.shekhawat8@gmail.com>
2024-10-15 15:22:46 +0200

    Genesis - init commit

---
+++ funds-transfer/src/main/java/com/finance/app/fundstransfer/FundsTransferApplication.java
@@ -0,0 +1,14 @@
+package com.finance.app.fundstransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundsTransferApplication.class, args);
+		System.out.println("Funds Transfer app!!!");
+	}
+
+}
