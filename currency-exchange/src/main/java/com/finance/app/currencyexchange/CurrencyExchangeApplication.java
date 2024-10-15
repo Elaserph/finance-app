3e60a80d327d303abe3a746882830ab39e8316cc
shreepal shekhawat <shreepal.shekhawat8@gmail.com>
2024-10-15 15:55:48 +0200

    Cleanup

--- currency-exchange/src/main/java/com/finance/app/currencyexchange/CurrencyExchangeApplication.java
+++ currency-exchange/src/main/java/com/finance/app/currencyexchange/CurrencyExchangeApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurrencyExchangeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeApplication.class, args);
-		System.out.println("Currency Exchange app!!!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeApplication.class, args);
+        System.out.println("Currency Exchange app!!!");
+    }
 
 }
--- funds-transfer/src/main/java/com/finance/app/fundstransfer/FundsTransferApplication.java
+++ funds-transfer/src/main/java/com/finance/app/fundstransfer/FundsTransferApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundsTransferApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FundsTransferApplication.class, args);
-		System.out.println("Funds Transfer app!!!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FundsTransferApplication.class, args);
+        System.out.println("Funds Transfer app!!!");
+    }
 
 }
