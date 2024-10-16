f8009ab6843280e1face44310d6d65e142cdb348
shreepal shekhawat <shreepal.shekhawat8@gmail.com>
2024-10-16 23:56:52 +0200

    Create dev profile and refactor CustomErrorController

--- currency-exchange/src/main/java/com/finance/app/currencyexchange/controller/CustomErrorController.java
+++ currency-exchange/src/main/java/com/finance/app/currencyexchange/controller/CustomErrorController.java
@@ -3,13 +3,13 @@ package com.finance.app.currencyexchange.controller;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         return status != null ? "Error with status code: " + Integer.valueOf(status.toString()) : "Unknown error";
